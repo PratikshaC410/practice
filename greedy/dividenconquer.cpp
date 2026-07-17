323f9d14beb26c1be1dd65e7e3e77fb0ee5a3460
PratikshaC410 <pratiksha42719@gmail.com>
2026-07-17 20:58:30 +0530

    divide and conquer greedy approach

---
+++ greedy/dividenconquer.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+void solve()
+{
+    int x, y;
+    cin >> x >> y;
+
+    if (x % y == 0)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
