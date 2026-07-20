f73f731d31ee503717f2aa3022ff1627bc1d7026
PratikshaC410 <pratiksha42719@gmail.com>
2026-07-20 09:33:21 +0530

    maximum repeating character

---
+++ dp/maxrepeatingstr.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int maxRepeating(string sequence, string word)
+    {
+        int count = 0;
+        string temp = word;
+
+        while (sequence.find(temp) < sequence.size())
+        {
+            count++;
+            temp += word;
+        }
+
+        return count;
+    }
+};
