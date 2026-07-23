e04ecc843b16bcf7da45147f782e0da9343ab461
PratikshaC410 <pratiksha42719@gmail.com>
2026-07-23 10:55:42 +0530

    sum count of subbarrays

---
+++ ARRAYS/sumcount.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+class Solution
+{
+public:
+    int sumCounts(vector<int> &nums)
+    {
+        int n = nums.size();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            unordered_set<int> st;
+
+            for (int j = i; j < n; j++)
+            {
+                st.insert(nums[j]);
+
+                int cnt = st.size();
+                ans += cnt * cnt;
+            }
+        }
+
+        return ans;
+    }
+};
