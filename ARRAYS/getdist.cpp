1951d1864c106def2da35dbe87a0d4854c386b43
PratikshaC410 <pratiksha42719@gmail.com>
2026-07-24 09:09:21 +0530

    getting the distancs of same arr ele optimised soln

--- ARRAYS/getdist.cpp
+++ ARRAYS/getdist.cpp
@@ -1,26 +1,44 @@
+#include <iostream>
 class Solution
 {
 public:
     vector<long long> getDistances(vector<int> &arr)
     {
+
         int n = arr.size();
-        vector<long long> ans(n, 0);
+        vector<long long> ans(n);
+
+        unordered_map<int, vector<int>> mp;
 
         for (int i = 0; i < n; i++)
         {
+            mp[arr[i]].push_back(i);
+        }
 
-            long long sum = 0;
+        for (auto &it : mp)
+        {
 
-            for (int j = 0; j < n; j++)
+            vector<int> &v = it.second;
+            int m = v.size();
+
+            vector<long long> prefix(m + 1, 0);
+
+            for (int i = 0; i < m; i++)
             {
-
-                if (i != j && arr[i] == arr[j])
-                {
-                    sum += abs(i - j);
-                }
+                prefix[i + 1] = prefix[i] + v[i];
             }
 
-            ans[i] = sum;
+            for (int i = 0; i < m; i++)
+            {
+
+                long long left = 1LL * i * v[i] - prefix[i];
+
+                long long right =
+                    (prefix[m] - prefix[i + 1]) -
+                    1LL * (m - i - 1) * v[i];
+
+                ans[v[i]] = left + right;
+            }
         }
 
         return ans;
