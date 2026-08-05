c109c6bc6f4097fb60644b4858eecddb1baf4810
PratikshaC410 <pratiksha42719@gmail.com>
2026-08-05 10:15:25 +0530

    tournament

---
+++ ARRAYS/game.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << 2 * n - 1 << "\n";
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
