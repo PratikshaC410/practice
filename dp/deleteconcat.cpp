8ca1ecd72f8ddf40c14cbf3d3b1d2ad4600497d0
PratikshaC410 <pratiksha42719@gmail.com>
2026-08-10 09:45:23 +0530

    delete and concatenate finding the max score

--- dp/deleteconcat.cpp
+++ dp/deleteconcat.cpp
@@ -8,8 +8,7 @@ void solve()
 {
     int n;
     long long c;
-    if (!(cin >> n >> c))
-        return;
+    cin >> n >> c;
 
     vector<long long> a(n);
     for (int i = 0; i < n; ++i)
@@ -20,32 +19,20 @@ void solve()
     vector<long long> dp(n + 1, -1e18);
     dp[0] = 0;
 
-    long long max_odd = -1e18;
-    long long max_even = -1e18;
-
     for (int i = 1; i <= n; ++i)
     {
         dp[i] = max(dp[i], dp[i - 1] + a[i - 1] - c);
         if (i >= 2)
         {
-            dp[i] = max(dp[i], dp[i - 2] + max(a[i - 1], a[i - 2]) - c);
-        }
-
-        if (i >= 1)
-        {
-            max_odd = max(max_odd, dp[i - 1] + a[i - 1] - c);
-            if (i >= 2)
-            {
-                max_even = max(max_even, dp[i - 2] + max(a[i - 1], a[i - 2]) - c);
-            }
+            long long max_two = max(a[i - 1], a[i - 2]);
+            dp[i] = max(dp[i], dp[i - 2] + max_two - c);
         }
 
         if (i >= 3)
         {
-            dp[i] = max(dp[i], max_odd + a[i - 1] - c);
+            long long max_three = max({a[i - 1], a[i - 2], a[i - 3]});
+            dp[i] = max(dp[i], dp[i - 3] + max_three - 2 * c);
         }
-
-        max_odd = max(max_odd - c, dp[i - 1] + a[i - 1] - c);
     }
 
     cout << dp[n] << "\n";
