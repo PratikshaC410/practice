6373af8e4383d5dafd28a35101f0e1a1d1a956d1
PratikshaC410 <pratiksha42719@gmail.com>
2026-08-10 10:42:08 +0530

    finding the possible bitset permutation

--- greedy/bitsetpermutation.cpp
+++ greedy/bitsetpermutation.cpp
@@ -1,88 +1,71 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-void solve()
-{
-    int n, k;
-    cin >> n >> k;
-    string s;
-    cin >> s;
-
-    int consecutive_ones = 0;
-    for (char c : s)
-    {
-        if (c == '1')
-        {
-            consecutive_ones++;
-            if (consecutive_ones >= k)
-            {
-                cout << "NO\n";
-                return;
-            }
-        }
-        else
-        {
-            consecutive_ones = 0;
-        }
-    }
-
-    cout << "YES\n";
-    vector<int> p(n);
-
-    int l = 0;
-    while (l < n)
-    {
-        if (s[l] == '0')
-        {
-            p[l] = l + 1;
-            l++;
-        }
-        else
-        {
-            int r = l;
-            while (r < n && s[r] == '1')
-            {
-                r++;
-            }
-            if (r < n)
-            {
-                for (int i = l; i <= r; ++i)
-                {
-                    p[i] = (i == r) ? l + 1 : i + 2;
-                }
-                l = r + 1;
-            }
-            else
-            {
-                for (int i = l - 1; i < n; ++i)
-                {
-                    p[i] = (i == l - 1) ? n : i;
-                }
-                l = n;
-            }
-        }
-    }
-
-    for (int i = 0; i < n; ++i)
-    {
-        cout << p[i] << (i == n - 1 ? "" : " ");
-    }
-    cout << "\n";
-}
 
 int main()
 {
 
-    int t;
-    if (cin >> t)
+    int T;
+    cin >> T;
+
+    while (T--)
     {
-        while (t--)
+        int n, k;
+        cin >> n >> k;
+
+        string s;
+        cin >> s;
+
+        bool ok = true;
+
+        int ones = 0;
+        for (char c : s)
+            ones += (c == '1');
+
+        int cur = 0;
+        for (int r = 0; r < n; r++)
         {
-            solve();
+            if (s[r] == '1')
+                cur++;
+
+            if (r >= k && s[r - k] == '1')
+                cur--;
+
+            if (r >= k - 1 && cur == k)
+            {
+                ok = false;
+                break;
+            }
+        }
+
+        if (!ok)
+        {
+            cout << "NO\n";
+            continue;
+        }
+
+        vector<int> p(n);
+
+        int small = 1;
+        int large = ones + 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '1')
+                p[i] = small++;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '0')
+                p[i] = large++;
+        }
+
+        cout << "YES\n";
+        for (int i = 0; i < n; i++)
+        {
+            cout << p[i] << (i + 1 == n ? '\n' : ' ');
         }
     }
+
     return 0;
 }
