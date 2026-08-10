246185984bc80b6dd87c93564739036d584a9954
PratikshaC410 <pratiksha42719@gmail.com>
2026-08-10 10:02:13 +0530

    delete and concatenate finding the max score

--- dp/deleteconcat.cpp
+++ dp/deleteconcat.cpp
@@ -1,53 +1,40 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-void solve()
-{
-    int n;
-    long long c;
-    cin >> n >> c;
-
-    vector<long long> a(n);
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> a[i];
-    }
-
-    vector<long long> dp(n + 1, -1e18);
-    dp[0] = 0;
-
-    for (int i = 1; i <= n; ++i)
-    {
-        dp[i] = max(dp[i], dp[i - 1] + a[i - 1] - c);
-        if (i >= 2)
-        {
-            long long max_two = max(a[i - 1], a[i - 2]);
-            dp[i] = max(dp[i], dp[i - 2] + max_two - c);
-        }
-
-        if (i >= 3)
-        {
-            long long max_three = max({a[i - 1], a[i - 2], a[i - 3]});
-            dp[i] = max(dp[i], dp[i - 3] + max_three - 2 * c);
-        }
-    }
-
-    cout << dp[n] << "\n";
-}
 
 int main()
 {
 
-    int t;
-    if (cin >> t)
+    int T;
+    cin >> T;
+
+    while (T--)
     {
-        while (t--)
+        int n;
+        long long c;
+        cin >> n >> c;
+
+        vector<long long> d(n);
+        long long ans = 0;
+
+        for (int i = 0; i < n; i++)
         {
-            solve();
+            long long a;
+            cin >> a;
+
+            ans += a - c;
+            d[i] = max(0LL, c - a);
         }
+
+        sort(d.begin(), d.end(), greater<long long>());
+
+        int m = n / 2;
+        for (int i = 0; i < m; i++)
+        {
+            ans += d[i];
+        }
+
+        cout << ans << '\n';
     }
+
     return 0;
 }
