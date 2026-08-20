82862755e2acbef04f84f85893d2aae30987e28e
PratikshaC410 <pratiksha42719@gmail.com>
2026-08-20 01:30:10 -0300

    token games with  turn wise

---
+++ ARRAYS/tokengame.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve()
+{
+    vector<int> tokens(3);
+    cin >> tokens[0] >> tokens[1] >> tokens[2];
+
+    int rounds = 0;
+
+    while (true)
+    {
+        sort(tokens.begin(), tokens.end());
+
+        if (tokens[0] == tokens[1] || tokens[1] == tokens[2] || tokens[0] == tokens[2])
+        {
+            break;
+        }
+
+        tokens[0] += 1;
+        tokens[2] -= 1;
+        rounds++;
+    }
+
+    cout << rounds << "\n";
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
