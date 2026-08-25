c95eb09ac13696b11065bfd7d18853ae81b1edfa
PratikshaC410 <pratiksha42719@gmail.com>
2026-08-25 08:29:16 +0530

    splitting the multiset into k

---
+++ greedy/multiset.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    cout << (n - 1 + k - 2) / (k - 1) << "\n";
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
