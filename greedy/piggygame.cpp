801f93d9a3da200d50bc82840962f0803c449d18
PratikshaC410 <pratiksha42719@gmail.com>
2026-08-26 10:03:50 +0530

    piggy game

---
+++ greedy/piggygame.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    cout << a[n / 2] << "\n";
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
