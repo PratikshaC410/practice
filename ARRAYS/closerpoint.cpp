d2ce2b9fa38b43e32207ba4c44b63c1b70fb38f3
PratikshaC410 <pratiksha42719@gmail.com>
2026-09-02 18:10:42 +0530

    closer points finding

---
+++ ARRAYS/closerpoint.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> x(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> x[i];
+    }
+
+    if (n == 2 && x[1] - x[0] > 1)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
