53db906098e9af22997d4ccd75a0b309bfd61851
PratikshaC410 <pratiksha42719@gmail.com>
2026-09-14 18:34:24 +0530

    finding the perfect num

--- ARRAYS/perfectnum.cpp
+++ ARRAYS/perfectnum.cpp
@@ -3,21 +3,23 @@ class Solution
 public:
     bool checkPerfectNumber(int num)
     {
-        int sum = 0;
-        for (int i = 1; i < num; i++)
+        if (num <= 1)
+            return false;
+
+        int sum = 1;
+
+        for (int i = 2; i * i <= num; i++)
         {
             if (num % i == 0)
             {
-                sum = sum + i;
+                sum += i;
+                if (i * i != num)
+                {
+                    sum += num / i;
+                }
             }
         }
-        if (num == sum)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+
+        return sum == num;
     }
 };
