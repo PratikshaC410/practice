6be6ddec34399b27bcd3fbccb78238d632e779ad
PratikshaC410 <pratiksha42719@gmail.com>
2026-09-15 18:12:03 +0530

    finding the sum of nodes with even grandparents

---
+++ binarytrees/sumofevengrandparents.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+private:
+    int dfs(TreeNode *node, TreeNode *parent, TreeNode *grandparent)
+    {
+        if (node == NULL)
+            return 0;
+
+        int sum = 0;
+
+        if (grandparent != NULL && grandparent->val % 2 == 0)
+        {
+            sum += node->val;
+        }
+
+        sum += dfs(node->left, node, parent);
+        sum += dfs(node->right, node, parent);
+
+        return sum;
+    }
+
+public:
+    int sumEvenGrandparent(TreeNode *root)
+    {
+        return dfs(root, NULL, NULL);
+    }
+};
