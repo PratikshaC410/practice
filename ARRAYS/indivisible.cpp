62a46dbdd4471114fcfd52d7f535ef345d355393
PratikshaC410 <pratiksha42719@gmail.com>
2026-09-22 19:45:33 +0530

    indivisible elements

---
+++ ARRAYS/indivisible.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (n % 2 == 1) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        for (int i = 1; i <= n; i += 2) {
+            cout << i + 1 << " " << i << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
