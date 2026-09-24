1f64168e8cd86bf5ca1376ca32ddbc04fc55b17a
PratikshaC410 <pratiksha42719@gmail.com>
2026-09-24 19:22:02 +0530

    piling the books and finding the max books

---
+++ math/alicebooks.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    long long ans = a[n - 1] + a[n - 2];
+    cout << ans << "\n";
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
