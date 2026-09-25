30dc293cebf4d2921fd4d428f1e9bf934cf03d02
PratikshaC410 <pratiksha42719@gmail.com>
2026-09-25 19:24:47 +0530

    checking if the password is strong

---
+++ Strings/checkpassword.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    if (is_sorted(s.begin(), s.end()))
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
